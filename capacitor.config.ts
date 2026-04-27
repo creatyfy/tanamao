b65ffea892b40ab68012b6e69ee42bca3ad43abf
creatyfy <i9plex@gmail.com>
2026-04-27 17:53:08 -0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.tanamao.delivery',
+  appId: 'com.tanamaoc.delivery',
   appName: 'Tá Na Mão',
   webDir: 'dist',
   android: {
